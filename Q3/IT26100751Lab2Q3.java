ec8ec81717993e70dd2f12b243767ecd4c200688
IT26100751 <it26100751@my.sliit.lk>
2026-06-15 23:05:57 +0530

    Add IT26100751Lab2Q3 to Q3

---
+++ Q3/IT26100751Lab2Q3.java
@@ -0,0 +1,13 @@
+public class IT26100751Lab2Q3{
+    public static void main(String[] args) {
+
+        double sideA = 3;
+        double sideB = 4;
+
+        double hypotenuse = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+
+        System.out.println("Side A: " + sideA);
+        System.out.println("Side B: " + sideB);
+        System.out.println("Hypotenuse: " + hypotenuse);
+    }
+}
